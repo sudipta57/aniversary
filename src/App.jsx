11ae74c60a6405434836ce0176086ac78b1868a7
sudipta57 <144714227+sudipta57@users.noreply.github.com>
2024-10-20 09:53:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import { useState } from "react";
 import Timer from "./component/Timer";
-import MainPage from "./component/MainPage.jsx";
+import MainPage from "./component/Mainpage.jsx";
 
 const App = () => {
   const [showMainPage, setShowMainPage] = useState(false);
